b5542c362b8f3c8e89b37466e887e83bb2614ae3
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2022-06-24 00:18:33 -0700

    Update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 import os
 
-
 def analyzeFile(input, output):
     writer = csv.writer(output)
     for line in input:
@@ -18,16 +17,18 @@ def analyzeFile(input, output):
                 label = ""
                 data =  ""
 
-def analyzeDirectory(directory, output):
+def analyzeDirectory(directory):
     for file in os.listdir(directory):
-        if(os.path.isfile(os.path.join(directory,file))):
-            input_file = open(os.path.join(directory,file))
-            output_file = open(output, "a+", encoding='UTF8', newline='')
-            analyzeFile(input_file, output_file)
-        elif(os.path.isdir(os.path.join(directory,file))):
-            analyzeDirectory(os.path.join(directory,file), output)
+        if "txt_out" not in file:
+            output = file[0:(len(file) - 4)] + "_output.csv"
+            if(os.path.isfile(os.path.join(directory,file))):
+                input_file = open(os.path.join(directory,file))
+                output_file = open(output, "a+", encoding='UTF8', newline='')
+                analyzeFile(input_file, output_file)
+            elif(os.path.isdir(os.path.join(directory,file))):
+                analyzeDirectory(os.path.join(directory,file))
 
 # note that the script will append to the output file parameter, so ensure
 # that the file is either empty or only has relevant values within it before
 # running this script on it
-analyzeDirectory("analysis", "output_1.csv")
+analyzeDirectory("analysis")
--- parse_blast_output.sh
+++ parse_blast_output.sh
@@ -1,5 +1,4 @@
 ###-----------
-cd /media/shraddha/MyPassport/NonExomics/Data/cancer/Experimental_validation/siRNAdesign/Parth18onwards/T18-84SiRNABLAST
 
 mkdir analysis
 for eachfile in *-Alignment.txt
@@ -13,10 +12,4 @@ do
     	rm tempp_*
 done
 
-#### Checking few cases where the number of mismatches in plus/Plus orientation is more than 16 - apart from target of interest.
-
-for eachfile in T35CD.Ri.407915.13.1_8R0UJGDC016-Alignment.txt T24CD.Ri.407850.13.25_8A1HB23T01N-Alignment.txt T39CD.Ri.407921.13.17_8R85TJ1T01N-Alignment.txt T45CD.Ri.407974.13.24_8W2JHPB9016-Alignment.txt T45CD.Ri.407974.13.26_8W2KAX1A016-Alignment.txt T52CD.Ri.408001.13.7_8Z7SJ876016-Alignment.txt
-do
-	sed -e '1,/Alignments:/d' $eachfile > tempp_alignments.txt
-	awk -F'\n' -v RS='>' -v ORS= '$1 !~  /Homo sapiens.*GRCh38/ {print RS $0}' tempp_alignments.txt > analysis/"$eachfile"_crosscheck
-done
+python3 main.py
